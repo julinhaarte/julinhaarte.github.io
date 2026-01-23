bbc64d1a2cf9ad10b434d8242a579d6c82e87a61
Júlia Soares <143275548+julinhaarte@users.noreply.github.com>
2026-01-23 00:03:14 -0300

    fix email

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-(function(){
-    emailjs.init('SM_xuvN0sBrOJni4v'); 
-})();
+document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init('SM_xuvN0sBrOJni4v');
 
-document.getElementById('emailForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+    document.getElementById('emailForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    const emailParams = {
-        from_email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
+        const emailParams = {
+            from_email: document.getElementById('email').value,
+            message: document.getElementById('message').value
+        };
 
-    emailjs.send('service_d9pm57j', 'template_0o0sstm', emailParams)
-        .then(function(response) {
-            alert('Eba! E-mail enviado! :D');
-        }, function(error) {
-            alert('Vixi! Deu ruim no envio! :(');
-        });
+        emailjs.send('service_d9pm57j', 'template_0o0sstm', emailParams)
+            .then(function(response) {
+                alert('Eba! E-mail enviado! :D');
+            }, function(error) {
+                alert('Vixi! Deu ruim no envio! :(');
+            });
+    });
 });
