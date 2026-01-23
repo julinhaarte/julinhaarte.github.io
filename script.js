1e64e67b97c948a21d8c97c3d5c9bb263046444a
Júlia Soares <143275548+julinhaarte@users.noreply.github.com>
2026-01-22 23:46:31 -0300

    fix email

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('emailForm').addEventListener('submit', function(event) 
         message: document.getElementById('message').value
     };
 
-    emailjs.send('julinhaarte-portfoliosit', 'template_r42q5p7', emailParams)
+    emailjs.send('julinhaarte-portfoliosit', 'template_0o0sstm', emailParams)
         .then(function(response) {
             alert('Eba! E-mail enviado! :D');
         }, function(error) {
